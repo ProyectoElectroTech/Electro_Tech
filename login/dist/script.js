87b546ac97f08e0b05ac440968dd282c2812a9e9
Abner Medina <amedinac@miumg.edu.gt>
2023-11-02 17:02:24 -0600

    :closed_lock_with_key: validar usuarios de accreso

--- login/dist/script.js
+++ login/dist/script.js
@@ -16,11 +16,33 @@ signupLink.onclick = () => {
   signupBtn.click();
   return false;
 };
-function redirectToInicio() {
+/*function redirectToInicio() {
 
   window.location.href = "/index.html";
+}*/
+
+function validar(){
+  var emaillog=document.getElementById("emaillog").value;
+  var passlog=document.getElementById("passlog").value;
+  if(emaillog=="Admin" && passlog=="umg123"){
+    alert("Ingreso Admin");
+    window.location.href = "/Admin/index.html";
+    return false;
+  }if(emaillog=="Abnermedina@gmail.com" && passlog=="abcd123-"){
+    alert("Ingreso "+emaillog);
+    window.location.href = "/index.html";
+    return false;
+  }else{
+    attempt--;
+  }
+  alert("Te quedan " +attempt+ " intentos")
+  if(attempt == 0){
+    document.getElementById("emaillog").disable=true;
+    document.getElementById("passlog").disable=true;
+    document.getElementById("indexBtn").disable=true;
+  }
 }
 
 // Asocia la función con el evento click del botón
-const redirigirBtn = document.getElementById("indexBtn");
-redirigirBtn.addEventListener("click", redirectToInicio);
+/*const redirigirBtn = document.getElementById("indexBtn");
+redirigirBtn.addEventListener("click", redirectToInicio);*/
